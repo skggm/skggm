0a4c068b93c134774245710ca984f61ddc2561e2
Jaska <jlaska@gmail.com>
2016-06-11 19:12:16 -0700

    Reproduce original pyquick tests in this framework

--- tests/quic_test.py
+++ tests/quic_test.py
@@ -1,16 +1,15 @@
+import os
 import numpy as np
 import pytest
+from scipy.io import loadmat
 
 from sklearn.utils.testing import assert_array_almost_equal
 from sklearn.utils.testing import assert_raises
+from sklearn.utils.testing import assert_allclose
 
 from sklearn import datasets
 
 from ..quic import QUIC
-
-X = datasets.load_diabetes().data
-X = np.dot(X, X.T)
-n_samples, n_features = X.shape
 
 
 class TestQUIC(object):
@@ -34,6 +33,9 @@ class TestQUIC(object):
         '''
         Just tests inputs/outputs (not validity of result).
         '''
+        X = datasets.load_diabetes().data
+        X = np.dot(X, X.T)
+
         quic = QUIC(**params_in)
         quic.fit(X)
         
@@ -51,6 +53,8 @@ class TestQUIC(object):
         '''
         Test behavior of invalid inputs.
         '''
+        X = datasets.load_diabetes().data
+        X = np.dot(X, X.T)
         quic = QUIC(method='unknownmethod')
         assert_raises(NotImplementedError, quic.fit, X)
 
@@ -59,3 +63,45 @@ class TestQUIC(object):
         data = datasets.load_diabetes().data
         quic = QUIC()
         assert_raises(ValueError, quic.fit, data)
+
+
+    @pytest.mark.parametrize("params_in, expected", [
+        ({
+            'lam': 0.5,
+            'max_iter': 100,
+        }, [923.1042]),
+        ({
+            'lam': 1.0,
+            'mode': 'path',
+            'path': np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5]),
+            'tol': 1e-16,
+            'max_iter': 100,
+        }, [1171.6578, 1136.1222, 1097.9438, 1053.0555, 995.6587, 923.1042]),
+        ({
+            'lam': 0.5,
+            'mode': 'trace',
+            'tol': 1e-16,
+            'max_iter': 11,
+        }, [993.2862, 965.4918, 927.3593, 923.3665, 923.1369, 923.1083, 923.1045, 923.1042, 923.1042, 923.1042, 923.1042]),
+    ])
+    def test_ER_692(self, params_in, expected):
+        '''
+        Requires that tests/ER_692.mat exists. 
+        It can be found in the MEX package archive from the [QUIC].
+        http://www.cs.utexas.edu/~sustik/QUIC/
+        
+        Reproduces tests from pyquic: https://github.com/osdf/pyquic
+        '''
+        if not os.path.exists('tests/ER_692.mat'):
+            print ('''Requires the file tests/ER_692.mat - this can be obtained in the MEX archive at http://www.cs.utexas.edu/~sustik/QUIC/''')
+            assert False
+
+        data = loadmat('tests/ER_692.mat')['S']
+        X = np.zeros(data.shape)
+        X[:] = data
+
+        quic = QUIC(**params_in)
+        quic.fit(X)
+
+        assert_allclose(quic.opt_, expected, rtol=1e-2)
+
