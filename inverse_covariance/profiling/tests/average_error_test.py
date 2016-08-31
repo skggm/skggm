Fix average error test for changed k

inverse_covariance/profiling/tests/average_error_test.py
@@ -39,13 +39,15 @@ class TestAverageError(object):
         ae = AverageError(**params_in)
         ae.fit(X)
 
+        num_k = 3
+
         assert np.sum(ae.error_fro_.flat) > 0
-        assert ae.error_fro_.shape == (5, ae.n_grid_points)
+        assert ae.error_fro_.shape == (num_k, ae.n_grid_points)
         assert np.sum(ae.error_supp_.flat) > 0
-        assert ae.error_supp_.shape == (5, ae.n_grid_points)
+        assert ae.error_supp_.shape == (num_k, ae.n_grid_points)
         assert np.sum(ae.error_fp_.flat) > 0
-        assert ae.error_fp_.shape == (5, ae.n_grid_points)
+        assert ae.error_fp_.shape == (num_k, ae.n_grid_points)
         assert np.sum(ae.error_fn_.flat) > 0
-        assert ae.error_fn_.shape == (5, ae.n_grid_points)
-        assert len(ae.ks_) == 5
+        assert ae.error_fn_.shape == (num_k, ae.n_grid_points)
+        assert len(ae.ks_) == num_k
         assert len(ae.grid_) == ae.n_grid_points