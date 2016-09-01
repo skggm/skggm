d6239b71fdde7ec4a243ca9bc3d3e7b145aa0ca1
Jaska <jlaska@gmail.com>
2016-09-01 10:46:32 -0700

    I believe this is the setup config that works

--- inverse_covariance/pyquic/setup.py
+++ inverse_covariance/pyquic/setup.py
@@ -9,27 +9,30 @@ if platform.system() == 'Darwin':
     extra_compile_args = ['-I/System/Library/Frameworks/vecLib.framework/Headers']
     if 'ppc' in platform.machine():
         extra_compile_args.append('-faltivec')
-        
-    ext_modules = [Extension(
-        name="pyquic",
-        sources=["QUIC.C", "pyquic.pyx"],
-        include_dirs = [numpy.get_include()],
-        extra_compile_args=extra_compile_args,
-        extra_link_args=["-Wl,-framework", "-Wl,Accelerate"],
-        language="c++"
-        )]
-else:
-    ext_modules = [Extension(
-        name="pyquic",
-        sources=["QUIC.C", "pyquic.pyx"],
-        include_dirs = [numpy.get_include(), "/usr/local/include"],
-        extra_compile_args=['-msse2', '-O2', '-fPIC', '-w'],
-        extra_link_args=["-llapack"],
-        language="c++"
-        )]
 
-setup(
-    name = "pyquic",
-    cmdclass = {"build_ext": build_ext},
-    ext_modules = ext_modules
+    extra_link_args = ["-Wl,-framework", "-Wl,Accelerate"]
+    include_dirs = [numpy.get_include()]
+        
+else:
+    include_dirs = [numpy.get_include(), "/usr/local/include"]
+    extra_compile_args=['-msse2', '-O2', '-fPIC', '-w']
+    extra_link_args=["-llapack"]
+
+
+
+ext_module = Extension(
+    name="pyquic",
+    sources=["QUIC.C", "pyquic.pyx"],
+    include_dirs=include_dirs,
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
+    language="c++"
+)
+
+
+if __name__ == "__main__":
+    setup(
+        name = "pyquic",
+        cmdclass = {"build_ext": build_ext},
+        ext_modules = [ext_module]
     )
--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
 from __future__ import print_function
 import sys
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-
-from build_deps import setup_pyquic
-
-with open('requirements.txt') as f:
-    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
-
-class InstallWithSetup(install):
-    def run(self):
-        install.run(self)
-        print('Compiling pyquic...')
-        setup_pyquic()
+from distutils.extension import Extension
+import platform
 
 try:
     import numpy
@@ -26,14 +16,51 @@ except ImportError:
     print('scipy is required during installation')
     sys.exit(1)
 
+import numpy as np
+
+if platform.system() == 'Darwin':
+    extra_compile_args = ['-I/System/Library/Frameworks/vecLib.framework/Headers']
+    if 'ppc' in platform.machine():
+        extra_compile_args.append('-faltivec')
+
+    extra_link_args = ["-Wl,-framework", "-Wl,Accelerate"]
+    include_dirs = [np.get_include()]
+        
+else:
+    include_dirs = [np.get_include(), "/usr/local/include"]
+    extra_compile_args=['-msse2', '-O2', '-fPIC', '-w']
+    extra_link_args=["-llapack"]
+
+
+# pyquic extension
+# --> inverse_covariance.pyquic.pyquic (contains func quic)
+ext_module = Extension(
+    name="pyquic.pyquic", # note: we ext_package= flag in setup()
+    sources=[
+        "inverse_covariance/pyquic/QUIC.C",
+        "inverse_covariance/pyquic/pyquic.pyx"],
+    include_dirs=include_dirs,
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
+    language="c++"
+)
+
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
 
 setup(name='skggm',
-      version='0.1.0',
-      description='Gaussian graphical models for scikit-learn.',
-      author='Jason Laska and Manjari Narayan',
-      packages=find_packages(),
-      install_requires=INSTALL_REQUIRES,
-      url='https://github.com/jasonlaska/skggm',
-      author_email='jlaska@gmail.com',
-      cmdclass={'install': InstallWithSetup},
+    version='0.1.0',
+    description='Gaussian graphical models for scikit-learn.',
+    author='Jason Laska and Manjari Narayan',
+    packages=[
+        'inverse_covariance',
+        'inverse_covariance.profiling',
+        'inverse_covariance.pyquic'],
+    install_requires=INSTALL_REQUIRES,
+    url='https://github.com/jasonlaska/skggm',
+    author_email='jlaska@gmail.com',
+    ext_package='inverse_covariance',
+    ext_modules=[ext_module],
 )
