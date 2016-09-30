33d1730b2fa7d1e3c2f64e4fabb885b834b309f2
Manjari Narayan (SMC 2) <manjari.narayan@gmail.com>
2016-09-30 01:55:59 +0000

    Removed private designation. Minor bugs in new approx_recovery

--- examples/simulate_networks.py
+++ examples/simulate_networks.py
@@ -128,7 +128,7 @@ def mvn(n_samples, n_features, cov, random_state=np.random.RandomState(2)):
 
 
 # MCMC Statistical Power
-def _plot_spower(results, grid, ks):
+def plot_spower(results, grid, ks):
     plt.figure()
     plt.plot(grid, results.T, lw=2)
     plt.xlabel('n/p (n_samples / n_features)')
@@ -140,7 +140,7 @@ def _plot_spower(results, grid, ks):
     plt.show()
 
 
-def _exact_support(prec, prec_hat):
+def exact_support(prec, prec_hat):
     # Q: why do we need something like this?, and why must eps be so big?
     # Q: can we automatically determine what this threshold should be?    
     eps = .2
@@ -153,12 +153,65 @@ def _exact_support(prec, prec_hat):
             np.nonzero(np.triu(prec,1).flat)[0],
             np.nonzero(np.triu(prec_hat,1).flat)[0])
 
+def count_support_diff(m, m_hat):
+    
+    n_features, _ = m.shape
 
-def _sp_trial(trial_estimator, n_samples, n_features, cov, adj, random_state):
+    m_no_diag = m.copy()
+    m_no_diag[np.diag_indices(n_features)] = 0
+    m_hat_no_diag = m_hat.copy()
+    m_hat_no_diag[np.diag_indices(n_features)] = 0
+
+    m_nnz = len(np.nonzero(m_no_diag.flat)[0])
+    m_hat_nnz = len(np.nonzero(m_hat_no_diag.flat)[0])
+
+    nnz_intersect = len(np.intersect1d(np.nonzero(m_no_diag.flat)[0],
+                                       np.nonzero(m_hat_no_diag.flat)[0]))
+    return m_nnz + m_hat_nnz - (2 * nnz_intersect)
+
+
+
+
+
+def approx_support(prec, prec_hat, prob=.01):
+    """
+    Returns True if model selection error is less than or equal to prob%
+    """        
+    #eps = .2
+    #prec_hat[np.abs(prec_hat) <= eps] = 0.0
+
+    # Why does np.nonzero/np.flatnonzero create so much problems? 
+    A = np.flatnonzero(np.triu(prec,1))
+    B = np.flatnonzero(np.triu(prec_hat,1))
+    ud = np.flatnonzero(np.triu(np.ones(prec.shape),1))
+    notA = np.setdiff1d(ud,A)
+
+    B_in_A_bool = np.in1d(B,A) # true positives
+    B_notin_A_bool = np.in1d(B,notA) # false positives
+    #print np.sum(B_in_A_bool), np.shape(A)[0]
+    #print np.sum(B_notin_A_bool), np.shape(notA)[0]
+    
+    if np.shape(A)[0]:
+        tpr = float(np.sum(B_in_A_bool))/len(A)
+        tnr = 1.0-tpr
+    else:
+        tpr = 0.0
+        tnr = 0.0        
+    if np.shape(notA)[0]:
+        fpr = float(np.sum(B_notin_A_bool))/len(notA)
+    else:
+        fpr = 0.0
+        
+    #print tnr,fpr
+    
+    return np.less_equal(tnr+fpr,prob), tpr, fpr
+
+
+def sp_trial(trial_estimator, n_samples, n_features, cov, adj, random_state):
     X = mvn(n_samples, n_features, cov,random_state=random_state)
     new_estimator = clone(trial_estimator)
     new_estimator.fit(X)
-    return _exact_support(adj, new_estimator.precision_)
+    return exact_support(adj, new_estimator.precision_)
 
 
 class StatisticalPower(object):
@@ -326,7 +379,7 @@ class StatisticalPower(object):
                     #max_nbytes=None,
                     #batch_size=1,
                 )(
-                    delayed(_sp_trial)(
+                    delayed(sp_trial)(
                         trial_estimator, n_samples, self.n_features, cov, adj, mcmc_prng
                     )
                     for nn in range(self.n_trials))
