2990aafdd839af51db1e10075cc52be68fc5f9b3
Jaska <jlaska@gmail.com>
2016-11-13 12:57:51 -0800

    Refactor some of the metrics to reuse the same code

--- inverse_covariance/profiling/metrics.py
+++ inverse_covariance/profiling/metrics.py
@@ -5,48 +5,11 @@ def exact_support(prec, prec_hat):
     #eps = np.finfo(prec_hat.dtype).eps # too small
     prec_hat[np.abs(prec_hat) <= eps] = 0.0
     
-    not_empty = np.count_nonzero(np.triu(prec,1))>0
+    not_empty = np.count_nonzero(np.triu(prec, 1)) > 0
     
     return not_empty & np.array_equal(
-            np.nonzero(np.triu(prec,1).flat)[0],
-            np.nonzero(np.triu(prec_hat,1).flat)[0])
-
-'''
-def count_support_diff(m, m_hat):
-    
-    n_features, _ = m.shape
-
-    m_no_diag = m.copy()
-    m_no_diag[np.diag_indices(n_features)] = 0
-    m_hat_no_diag = m_hat.copy()
-    m_hat_no_diag[np.diag_indices(n_features)] = 0
-
-    m_nnz = len(np.nonzero(m_no_diag.flat)[0])
-    m_hat_nnz = len(np.nonzero(m_hat_no_diag.flat)[0])
-
-    nnz_intersect = len(np.intersect1d(np.nonzero(m_no_diag.flat)[0],
-                                       np.nonzero(m_hat_no_diag.flat)[0]))
-    return m_nnz + m_hat_nnz - (2 * nnz_intersect)
-'''
-
-def support_diff(m, m_hat):
-    '''Count the number of different elements in the support in one triangle,
-    not including the diagonal. 
-    '''
-    n_features, _ = m.shape
-
-    m_no_diag = m.copy()
-    m_no_diag[np.diag_indices(n_features)] = 0
-    m_hat_no_diag = m_hat.copy()
-    m_hat_no_diag[np.diag_indices(n_features)] = 0
-
-    m_nnz = len(np.nonzero(m_no_diag.flat)[0])
-    m_hat_nnz = len(np.nonzero(m_hat_no_diag.flat)[0])
-
-    nnz_intersect = len(np.intersect1d(np.nonzero(m_no_diag.flat)[0],
-                                       np.nonzero(m_hat_no_diag.flat)[0]))
-    return (m_nnz + m_hat_nnz - (2 * nnz_intersect)) / 2.0
-
+            np.nonzero(np.triu(prec, 1).flat)[0],
+            np.nonzero(np.triu(prec_hat, 1).flat)[0])
 
 def approx_support(prec, prec_hat, prob=.01):
     """
@@ -82,9 +45,16 @@ def approx_support(prec, prec_hat, prob=.01):
     return np.less_equal(tnr+fpr,prob), tpr, fpr
 
 
-def _false_support(m, m_hat):
-    '''Count the number of false positive and false negatives supports in 
-    m_hat in one triangle, not including the diagonal.
+def _nonzero_intersection(m, m_hat):
+    '''Count the number of nonzeros
+
+    Returns:
+
+    m_nnz :  number of nonzeros in m (w/o diagonal)
+    
+    m_hat_nnz : number of nonzeros in m_hat (w/o diagonal)
+    
+    intersection_nnz : number of nonzeros in intersection of m/m_hat (w/o diagonal)
     '''
     n_features, _ = m.shape
 
@@ -93,12 +63,34 @@ def _false_support(m, m_hat):
     m_hat_no_diag = m_hat.copy()
     m_hat_no_diag[np.diag_indices(n_features)] = 0
 
-    m_nnz = len(np.nonzero(m_no_diag.flat)[0])
     m_hat_nnz = len(np.nonzero(m_hat_no_diag.flat)[0])
+    m_nnz = len(np.nonzero(m_no_diag.flat)[0])
 
-    nnz_intersect = len(np.intersect1d(np.nonzero(m_no_diag.flat)[0],
+    intersection_nnz = len(np.intersect1d(np.nonzero(m_no_diag.flat)[0],
                                        np.nonzero(m_hat_no_diag.flat)[0]))
 
-    false_positives = (m_hat_nnz - nnz_intersect) / 2.0
-    false_negatives = (m_nnz - nnz_intersect) / 2.0
-    return false_positives, false_negatives
+    return m_nnz, m_hat_nnz, intersection_nnz
+
+
+def support_false_positive_count(m, m_hat):
+    '''Count the number of false positive support elements in 
+    m_hat in one triangle, not including the diagonal.
+    '''
+    m_nnz, m_hat_nnz, intersection_nnz = _nonzero_intersection(m, m_hat)
+    return (m_hat_nnz - intersection_nnz) / 2.0
+
+
+def support_false_negative_count(m, m_hat):
+    '''Count the number of false negative support elements in 
+    m_hat in one triangle, not including the diagonal.
+    '''
+    m_nnz, m_hat_nnz, intersection_nnz = _nonzero_intersection(m, m_hat)
+    return (m_nnz - intersection_nnz) / 2.0
+
+
+def support_difference_count(m, m_hat):
+    '''Count the number of different elements in the support in one triangle,
+    not including the diagonal. 
+    '''
+    m_nnz, m_hat_nnz, intersection_nnz = _nonzero_intersection(m, m_hat)
+    return (m_nnz + m_hat_nnz - (2 * intersection_nnz)) / 2.0
