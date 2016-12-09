9d77c86205309ffe0269674cfdea84ea5361c1e3
Jaska <jlaska@gmail.com>
2016-12-08 20:31:02 -0800

    Add exception if graph is called with 1 block but chain_blocks is True.

--- inverse_covariance/profiling/graphs.py
+++ inverse_covariance/profiling/graphs.py
@@ -147,6 +147,10 @@ class Graph(object):
         self.seed = seed
         self.prng = np.random.RandomState(self.seed)
 
+        if n_blocks == 1 and chain_blocks:
+            raise ValueError("chain_blocks must be False when n_blocks is 1.")
+            return
+
     def to_precision(self, adjacency, weighted=True, rescale=True):
         if weighted:
             dd_adj = _to_diagonally_dominant_weighted(adjacency)
