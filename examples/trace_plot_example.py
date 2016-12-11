4bfbe0cde35d3a21cc1e5942c574a4b67dc37d06
Jaska <jlaska@gmail.com>
2016-12-11 13:46:00 -0800

    A lot of fixes to trace_plot: only plot lower triangle, only plot top n_edges edges, only plot non-zero paths.  Adds larger example to trace_plot_example.

--- examples/trace_plot_example.py
+++ examples/trace_plot_example.py
@@ -5,6 +5,9 @@ from sklearn.datasets import make_sparse_spd_matrix
 sys.path.append('..')
 from inverse_covariance import QuicGraphLasso
 from inverse_covariance.plot_util import trace_plot 
+from inverse_covariance.profiling import (
+    LatticeGraph,
+)
 
 
 def make_data(n_samples, n_features):
@@ -25,18 +28,40 @@ def make_data(n_samples, n_features):
     return X, cov, prec
 
 
+def make_data_banded(n_samples, n_features):
+    alpha = 0.1
+    cov, prec, adj = LatticeGraph(
+        n_blocks=2,
+        random_sign=True,
+        chain_blocks=True,
+        seed=1,
+    ).create(n_features, alpha)
+    prng = np.random.RandomState(2)
+    X = prng.multivariate_normal(np.zeros(n_features), cov, size=n_samples)
+    return X, cov, prec
+
+
 def show_quic_coefficient_trace(X):
-    path = np.logspace(np.log10(0.01), np.log10(1.0), num=50, endpoint=True)[::-1]
+    path = np.logspace(
+        np.log10(0.01), np.log10(1.0), num=50, endpoint=True
+    )[::-1]
     estimator = QuicGraphLasso(
             lam=1.0,
             path=path,
             mode='path')
     estimator.fit(X)
-    trace_plot(estimator.precision_, estimator.path_)
+    trace_plot(estimator.precision_, estimator.path_, n_edges=20)
 
 
 if __name__ == "__main__":
+    # example 1
     n_samples = 10
     n_features = 5
     X, cov, prec = make_data(n_samples, n_features)
+    show_quic_coefficient_trace(X)
+
+    # example 2
+    n_samples = 110
+    n_features = 100
+    X, cov, prec = make_data_banded(n_samples, n_features)
     show_quic_coefficient_trace(X)
--- inverse_covariance/plot_util.py
+++ inverse_covariance/plot_util.py
@@ -12,41 +12,54 @@ def _check_path(in_path):
     assert_array_equal(path, in_path)
 
 
-def trace_plot(results, path):
+def trace_plot(precisions, path, n_edges=20):
     """Plot the change in precision (or covariance) coefficients as a function 
-    of changing lambda and l1-norm.
+    of changing lambda and l1-norm.  Always ignores diagonals.
 
     Parameters
     -----------
-    results: array of len(path) of 2D ndarray, shape (n_features, n_features)
+    precisions : array of len(path) of 2D ndarray, shape (n_features, n_features)
         This is either precision_ or covariance_ from an InverseCovariance
         estimator in path mode, or a list of results for individual runs of 
         the GraphLasso.
 
-    path:  array of floats (descending)
+    path :  array of floats (descending)
         This is path of lambdas explored. 
+
+    n_edges :  int (default=20)
+        Max number of edges to plot for each precision matrix along the path.
+        Only plots the maximum magnitude values (evaluating the last precision 
+        matrix).
     """
     _check_path(path)
-    assert len(path) == len(results)
-    assert len(results) > 0
+    assert len(path) == len(precisions)
+    assert len(precisions) > 0
 
     path = np.array(path)
-    dim, _ = results[0].shape
+    dim, _ = precisions[0].shape
+
+    # determine which indices to track
+    base_precision = np.copy(precisions[-1])
+    base_precision[np.triu_indices(base_precision.shape[0])] = 0
+    sidx = np.argsort(np.abs(base_precision.flat))[::-1][: n_edges]
 
     # reshape data a bit:  
     # flatten each matrix into a column (so that coeffs are examples)
     # compute l1-norm of each column
     l1_norms = []
-    coeffs = np.zeros((dim**2, len(results)))
-    for ridx, result in enumerate(results):
-        coeffs[:, ridx] = result.flat 
+    coeffs = np.zeros((dim**2, len(precisions)))
+    for ridx, result in enumerate(precisions):
+        coeffs[sidx, ridx] = result.flat[sidx]
         l1_norms.append(np.linalg.norm(coeffs[:, ridx]))
+
+    # remove any zero rows
+    coeffs = coeffs[np.linalg.norm(coeffs, axis=1) > 1e-10, :]
 
     plt.figure()
 
     # show coefficients as a function of lambda
     plt.subplot(1, 2, 1)
-    for result in results:
+    for result in precisions:
         plt.plot(l1_norms, coeffs.T, lw=1)
 
     plt.xlim([np.min(l1_norms), np.max(l1_norms)])
@@ -56,7 +69,7 @@ def trace_plot(results, path):
     # show coefficients as a function of lambda
     log_path = np.log(path)
     plt.subplot(1, 2, 2)
-    for result in results:
+    for result in precisions:
         plt.plot(log_path, coeffs.T, lw=1)
 
     plt.xlim([np.min(log_path), np.max(log_path)])
