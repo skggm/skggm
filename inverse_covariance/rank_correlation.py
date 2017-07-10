2f622c0ac96d117f930397801580c5bb58a49842
mnarayan (SMC 2) <mnarayan@users.noreply.github.com>
2017-07-10 05:27:43 +0000

    Added references and kendall_correlation implementation

--- inverse_covariance/rank_correlation.py
+++ inverse_covariance/rank_correlation.py
@@ -1,7 +1,11 @@
 """Nonparametric rank correlation estimators as alternative to
  linear correlation estimators."""
 import numpy as np
-from scipy.stats import rankdata
+from scipy.stats import (
+    rankdata,
+    kendalltau,
+    weightedtau
+)
 
 
 def _compute_ranks(X):
@@ -27,7 +31,7 @@ def _compute_ranks(X):
     return Xrank
 
 
-def spearman_correlation(X,rowvar=False):
+def spearman_correlation(X, rowvar=False):
     """
     Computes the spearman correlation estimate.
     This is effectively a bias corrected pearson correlation
@@ -42,16 +46,31 @@ def spearman_correlation(X,rowvar=False):
     Returns
     -------
     rank_correlation
+
+    References
+    ----------
+
+    Xue, Lingzhou; Zou, Hui.
+    "Regularized rank-based estimation of high-dimensional
+    nonparanormal graphical models."
+    Ann. Statist. 40 (2012), no. 5, 2541--2571. doi:10.1214/12-AOS1041.
+
+    Liu, Han, Fang; Yuan, Ming; Lafferty, John; Wasserman, Larry.
+    "High-dimensional semiparametric Gaussian copula graphical models."
+    Ann. Statist. 40.4 (2012): 2293-2326. doi:10.1214/12-AOS1037
     """
+
     Xrank = _compute_ranks(X)
-    rank_correlation = np.corrcoef(Xrank, rowvar=False)
+    rank_correlation = np.corrcoef(Xrank, rowvar=rowvar)
 
     return 2 * np.sin(rank_correlation * np.pi / 6)
 
 
-def kendalltau_correlation(X):
+def kendalltau_correlation(X, rowvar=False, weighted=False):
     """
     Computes kendall's tau correlation estimate.
+    The option to use scipy.stats.weightedtau is not recommended
+    as the implementation does not appear to handle ties correctly.
 
     Parameters
     ----------
@@ -62,6 +81,33 @@ def kendalltau_correlation(X):
     Returns
     -------
     rank_correlation
+
+    References
+    ----------
+
+    Liu, Han, Fang; Yuan, Ming; Lafferty, John; Wasserman, Larry.
+    "High-dimensional semiparametric Gaussian copula graphical models."
+    Ann. Statist. 40.4 (2012): 2293-2326. doi:10.1214/12-AOS1037
+
+    Barber, Rina Foygel; Kolar, Mladen.
+    "ROCKET: Robust Confidence Intervals via Kendall's Tau
+    for Transelliptical Graphical Models."
+     arXiv:1502.07641
     """
 
-    pass
+    if(rowvar):
+        X = X.T
+
+    _, n_features = X.shape
+    rank_correlation = np.eye(n_features)
+    for row in np.arange(n_features):
+        for col in np.arange(1+row, n_features):
+            if(weighted):
+                rank_correlation[row, col], _ = weightedtau(
+                    X[:, row], X[:, col], rank=False)
+            else:
+                rank_correlation[row, col], _ = kendalltau(
+                    X[:, row], X[:, col])
+    rank_correlation = np.triu(rank_correlation, 1) + rank_correlation.T
+
+    return np.sin(rank_correlation * np.pi / 2)
