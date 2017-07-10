8fa91f8dd02685a021fdaf68eaef790d7030fdce
mnarayan (SMC 2) <mnarayan@users.noreply.github.com>
2017-07-10 02:15:24 +0000

    Simple test functions for spearman correlation added

---
+++ inverse_covariance/tests/rank_correlation_test.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from sklearn.utils.testing import assert_array_almost_equal
+
+from inverse_covariance import (
+    compute_ranks,
+    spearman_correlation,
+)
+
+Y = np.ones(shape=[10, 2])
+X = .001 * np.random.randn(20, 1)
+X = np.append(X, X, axis=1)
+
+
+def test_compute_ranks():
+    Y1 = 5.5 * Y
+    Y2 = compute_ranks(Y)
+    assert_array_almost_equal(Y1, Y2)
+
+
+def test_spearman_correlation():
+    A = np.ones(shape=[2, 2])
+    A2 = spearman_correlation(X)
+    assert_array_almost_equal(A, A2, decimal=3)
