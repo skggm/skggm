25c55d118d3659220652a858de2ba46435e8c8e2
mnarayan (SMC 2) <mnarayan@users.noreply.github.com>
2017-07-10 06:14:14 +0000

    Added a basic test for kendalltau_correlation

--- inverse_covariance/tests/rank_correlation_test.py
+++ inverse_covariance/tests/rank_correlation_test.py
@@ -5,6 +5,7 @@ from sklearn.utils.testing import assert_array_almost_equal
 from inverse_covariance.rank_correlation import (
     _compute_ranks,
     spearman_correlation,
+    kendalltau_correlation,
 )
 
 Y = np.ones(shape=[10, 2])
@@ -22,3 +23,13 @@ def test_spearman_correlation():
     A = np.ones(shape=[2, 2])
     A2 = spearman_correlation(X)
     assert_array_almost_equal(A, A2, decimal=3)
+
+
+def test_kendalltau_correlation():
+    A = np.ones(shape=[2, 2])
+    
+    A2 = kendalltau_correlation(X,weighted=False)
+    assert_array_almost_equal(A, A2, decimal=3)
+    
+    A3 = kendalltau_correlation(X,weighted=True)
+    assert_array_almost_equal(A, A3, decimal=3)
