c99736af6b0c96280f92434db0aad22cd572e1cc
Jaska <jlaska@gmail.com>
2017-07-15 18:40:07 -0700

    Add init_method comments to routines in quic_graph_lasso.

--- inverse_covariance/quic_graph_lasso.py
+++ inverse_covariance/quic_graph_lasso.py
@@ -201,8 +201,13 @@ class QuicGraphLasso(InverseCovarianceEstimator):
                   'kl', or 'quadratic'
         Used for computing self.score().
 
-    init_method : one of 'corrcoef', 'cov', or custom function
+    init_method : one of 'corrcoef', 'cov', 'spearman', 'kendalltau',
+        or a custom function.
         Computes initial covariance and scales lambda appropriately.
+        Using the custom function extends graphical model estimation to
+        distributions beyond the multivariate Gaussian.
+        The `spearman` or `kendalltau` options extend inverse covariance
+        estimation to nonparanormal and transelliptic graphical models.
         Custom function must return ((n_features, n_features) ndarray, float)
         where the scalar parameter will be used to scale the penalty lam.
 
@@ -465,8 +470,13 @@ class QuicGraphLassoCV(InverseCovarianceEstimator):
                   'kl', or 'quadratic'
         Used for computing self.score().
 
-    init_method : one of 'corrcoef', 'cov', or custom function
+    init_method : one of 'corrcoef', 'cov', 'spearman', 'kendalltau',
+        or a custom function.
         Computes initial covariance and scales lambda appropriately.
+        Using the custom function extends graphical model estimation to
+        distributions beyond the multivariate Gaussian.
+        The `spearman` or `kendalltau` options extend inverse covariance
+        estimation to nonparanormal and transelliptic graphical models.
         Custom function must return ((n_features, n_features) ndarray, float)
         where the scalar parameter will be used to scale the penalty lam.
 
@@ -772,8 +782,13 @@ class QuicGraphLassoEBIC(InverseCovarianceEstimator):
     verbose : integer
         Used in quic routine.
 
-    init_method : one of 'corrcoef', 'cov', or custom function
+    init_method : one of 'corrcoef', 'cov', 'spearman', 'kendalltau',
+        or a custom function.
         Computes initial covariance and scales lambda appropriately.
+        Using the custom function extends graphical model estimation to
+        distributions beyond the multivariate Gaussian.
+        The `spearman` or `kendalltau` options extend inverse covariance
+        estimation to nonparanormal and transelliptic graphical models.
         Custom function must return ((n_features, n_features) ndarray, float)
         where the scalar parameter will be used to scale the penalty lam.
 
