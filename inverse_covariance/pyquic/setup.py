113f9bb6a39d21d76819c78525c5ca88b3531a95
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2018-01-07 21:01:56 -0800

    Links to mkl libraries if present

--- inverse_covariance/pyquic/setup.py
+++ inverse_covariance/pyquic/setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import platform
+import os
 
 
 if platform.system() == 'Darwin':
@@ -16,9 +17,19 @@ if platform.system() == 'Darwin':
     include_dirs = [numpy.get_include()]
 
 else:
-    include_dirs = [numpy.get_include(), "/usr/local/include"]
+    if "MKLROOT" in os.environ:
+        include_dirs = [numpy.get_include(), "/usr/local/include", 
+        os.path.join(os.environ.get('MKLROOT'),'include')]
+    else:
+        include_dirs = [numpy.get_include(), "/usr/local/include"]
     extra_compile_args = ['-msse2', '-O2', '-fPIC', '-w']
-    extra_link_args = ["-llapack"]
+    if "MKLROOT" in os.environ:
+        extra_link_args = ["-L" + os.environ.get('MKLROOT') + 
+        " -lmkl_def -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_sequential" + \
+        " -lmkl_core -lmkl_gnu_thread -lmkl_lapack95_lp64"]
+    else:
+        extra_link_args = ["-llapack"]
+
 
 ext_module = Extension(
     name="pyquic",
