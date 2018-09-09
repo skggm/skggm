d2800fc78a0f384bd64f7096ed5cfd0067215ee0
Jaska <jlaska@gmail.com>
2018-09-09 15:34:01 -0700

    Bring back partial_fit capability, add tests, ask questions.

--- inverse_covariance/tests/two_way_standard_scaler_test.py
+++ inverse_covariance/tests/two_way_standard_scaler_test.py
@@ -1,9 +1,13 @@
+import pytest
 import numpy as np
 from scipy import sparse
+from sklearn.utils.testing import assert_raises, assert_allclose
+from sklearn.exceptions import NotFittedError
 
-from sklearn.utils.testing import assert_raises
-
-from inverse_covariance.two_way_standard_scaler import two_way_standardize
+from inverse_covariance.two_way_standard_scaler import (
+    two_way_standardize,
+    TwoWayStandardScaler,
+)
 
 
 def custom_init(n_rows, n_cols, with_mean=False):
@@ -22,13 +26,182 @@ def custom_init(n_rows, n_cols, with_mean=False):
     return mu + sqcov_rows * X * sqcov_cols
 
 
-def test_invalid_argument():
+def test_fit_exception_on_sparse_input():
     """
     Test behavior of invalid sparse data matrix inputs.
     """
     X = np.zeros(shape=(10, 10))
-    X_csc = sparse.csc_matrix(X)
-    assert_raises(TypeError, two_way_standardize, X_csc)
+    sparse_Xs = [sparse.csc_matrix(X), sparse.csr_matrix(X)]
+    for sparse_X in sparse_Xs:
+        scaler = TwoWayStandardScaler()
+        assert_raises(TypeError, scaler.fit, sparse_X)
 
-    X_csr = sparse.csr_matrix(X)
-    assert_raises(TypeError, two_way_standardize, X_csr)
+
+def test_transform_exception_not_fitted():
+    """
+    Test behavior of invalid sparse data matrix inputs.
+    """
+    scaler = TwoWayStandardScaler()
+    assert_raises(NotFittedError, scaler.transform, np.zeros(shape=(10, 10)))
+
+
+def test_transform_exception_on_sparse_input():
+    """
+    Test behavior of invalid sparse data matrix inputs.
+    """
+    X = np.zeros(shape=(10, 10))
+    sparse_Xs = [sparse.csc_matrix(X), sparse.csr_matrix(X)]
+    for sparse_X in sparse_Xs:
+        scaler = TwoWayStandardScaler()
+        scaler.fit(X)
+        assert_raises(TypeError, scaler.transform, sparse_X)
+
+
+@pytest.mark.parametrize(
+    "data, with_std, expected",
+    [
+        (
+            [[1, 0], [1, 0], [2, 1], [2, 1]],
+            True,  # with_std=True
+            [
+                [0.5, 0.5, 1.5, 1.5],  # row_mean_
+                [0.25, 0.25, 0.25, 0.25],  # row_var_
+                [1.5, 0.5],  # col_mean_
+                [0.25, 0.25],  # col_var_
+                [0.5, 0.5, 0.5, 0.5],  # row_scale_
+                [0.5, 0.5],  # col_scale_
+                [4],  # [n_rows_seen_]
+                [2],  # [n_cols_seen_]
+            ],
+        ),
+        (
+            [[1, 0], [1, 0], [2, 1], [2, 1]],
+            False,  # with_std=False
+            [
+                [0.5, 0.5, 1.5, 1.5],  # row_mean_
+                None,  # row_var_
+                [1.5, 0.5],  # col_mean_
+                None,  # col_var_
+                None,  # row_scale_
+                None,  # col_scale_
+                [4],  # [n_rows_seen_]
+                [2],  # [n_cols_seen_]
+            ],
+        ),
+    ],
+)
+def test_fit(data, with_std, expected):
+    scaler = TwoWayStandardScaler(with_std=with_std)
+    scaler.fit(data)
+    result = [
+        scaler.row_mean_,
+        scaler.row_var_,
+        scaler.col_mean_,
+        scaler.col_var_,
+        scaler.row_scale_,
+        scaler.col_scale_,
+        [scaler.n_rows_seen_],
+        [scaler.n_cols_seen_],
+    ]
+    assert_allclose(
+        [i for e in expected if e is not None for i in e],
+        [j for r in result if r is not None for j in r],
+    )
+
+
+@pytest.mark.parametrize(
+    "data, with_std, expected",
+    [
+        (
+            [
+                [[1, 0], [1, 0], [2, 1], [2, 1]],
+                [[1, 0], [1, 0], [2, 1], [2, 1]],
+                [[1, 0], [1, 0], [2, 1], [2, 1]],
+            ],  # multiple data examples for "online" estimation
+            True,  # with_std=True
+            [
+                [0.5, 0.5, 1.5, 1.5],  # row_mean_
+                [0.25, 0.25, 0.25, 0.25],  # row_var_
+                [1.5, 0.5],  # col_mean_
+                [0.25, 0.25],  # col_var_
+                [0.5, 0.5, 0.5, 0.5],  # row_scale_
+                [0.5, 0.5],  # col_scale_
+                [12],  # [n_rows_seen_]
+                [6],  # [n_cols_seen_]
+            ],
+        )
+    ],
+)
+def test_partial_fit(data, with_std, expected):
+    scaler = TwoWayStandardScaler(with_std=with_std)
+    for d in data:
+        scaler.partial_fit(d)
+
+    result = [
+        scaler.row_mean_,
+        scaler.row_var_,
+        scaler.col_mean_,
+        scaler.col_var_,
+        scaler.row_scale_,
+        scaler.col_scale_,
+        [scaler.n_rows_seen_],
+        [scaler.n_cols_seen_],
+    ]
+    print(result)
+    assert_allclose(
+        [i for e in expected if e is not None for i in e],
+        [j for r in result if r is not None for j in r],
+    )
+
+
+@pytest.mark.parametrize(
+    "n_rows, n_cols, with_mean, with_std, expected",
+    [
+        (
+            6,  # n_rows
+            2,  # n_cols
+            False,  # with_mean
+            True,  # with_std
+            [
+                [1.24852525, -0.47021609],
+                [-1.66629192, -3.38503326],
+                [0.46966753, -1.24907381],
+                [1.1966711, -0.52207024],
+                [0.96470187, -0.75403946],
+                [0.71346052, -1.00528082],
+            ],
+        ),
+        (
+            6,  # n_rows
+            2,  # n_cols
+            True,  # with_mean
+            False,  # with_std
+            [
+                [1.85393809, -1.85393809],
+                [-19.71891691, 19.71891691],
+                [13.72700391, -13.72700391],
+                [5.29676963, -5.29676963],
+                [-19.41773454, 19.41773454],
+                [18.25893982, -18.25893982],
+            ],
+        ),
+        (
+            6,  # n_rows
+            2,  # n_cols
+            True,  # with_mean
+            True,  # with_std
+            [
+                [-1.41421356, 1.41421356],
+                [-1.41421356, 1.41421356],
+                [0.70710678, -0.70710678],
+                [0.70710678, -0.70710678],
+                [0.70710678, -0.70710678],
+                [0.70710678, -0.70710678],
+            ],
+        ),
+    ],
+)
+def test_two_way_standardize(n_rows, n_cols, with_mean, with_std, expected):
+    X = custom_init(n_rows, n_cols, with_mean=with_mean)
+    result = two_way_standardize(X, with_mean=with_mean, with_std=with_std)
+    assert_allclose(result, expected)
--- inverse_covariance/two_way_standard_scaler.py
+++ inverse_covariance/two_way_standard_scaler.py
@@ -24,61 +24,69 @@ def two_way_standardize(
     """Standardize a two-dimensional data matrix along both axes.
     Center to the mean and component wise scale to unit variance.
     Read more in the :ref:`User Guide <preprocessing_scaler>`.
+
     Parameters
     ----------
     X : {array-like, sparse matrix}
         The data to center and scale.
+
     axis : int (0 by default)
         axis used to compute the means and standard deviations along. If 0,
         independently standardize each feature, otherwise (if 1) standardize
         each sample.
+
     with_mean : boolean, True by default
         Is always true for two-way standardize
+
     with_std : boolean, True by default
         If True, scale the data to unit variance (or equivalently,
         unit standard deviation).
+
     copy : boolean, optional, default True
         set to False to perform inplace row normalization and avoid a
         copy (if the input is already a numpy array or a scipy.sparse
         CSC matrix and if axis is 1).
+
     max_iter : int, optional (50 by default)
         Set the maximum number of iterations of successive normalization algorithm
+
     tol : float, optional (1e-6 by default)
         Set the convergence threshold for successive normalization
+
     Notes
     -----
-    This function invokes sklearn's scale function. Thus, the same restrictions
+    This function invokes sklearn's scale function, thus the same restrictions
     for scale, apply here as well.
-    This implementation will refuse to center scipy.sparse matrices
-    since it would make them non-sparse and would potentially crash the
-    program with memory exhaustion problems.
-    Instead the caller is expected to either set explicitly
-    `with_mean=False` (in that case, only variance scaling will be
-    performed on the features of the CSC matrix) or to call `X.toarray()`
-    if he/she expects the materialized dense array to fit in memory.
-    To avoid memory copy the caller should pass a CSC matrix.
+
+    The caller should pass a CSC matrix. The caller is expected to either set
+    explicitly `with_mean=False`(in that case, only variance scaling will be
+    performed on the features of the CSC matrix) or to call `X.toarray()` if
+    the array fits in memory.
+
     For a comparison of the different scalers, transformers, and normalizers,
     see sklearn documentation `examples/preprocessing/plot_all_scaling.py
+
     See also
     --------
-    StandardScaler: Performs scaling to unit variance using the``Transformer`` API
-        (e.g. as part of a preprocessing :class:`sklearn.pipeline.Pipeline`).
+    StandardScaler: Performs scaling to unit variance using the``Transformer`` API,
+    e.g. as part of a preprocessing :class:`sklearn.pipeline.Pipeline`.
     """
     n_rows, n_cols = np.shape(X)
 
-    err_norm = np.inf
+    row_polish = np.copy(X.T)
     rows_X = np.copy(X.T)
     cols_X = np.copy(X)
-    n_iter = 0
-    while n_iter <= max_iter and err_norm > tol:
-        col_polish = scale(row_polish.T, axis=1, with_mean=True, with_std=with_std)
-        row_polish = scale(col_polish.T, axis=1, with_mean=True, with_std=with_std)
 
-        err_norm_row = np.linalg.norm(rows_X - row_polish, "fro")
-        err_norm_col = np.linalg.norm(cols_X - col_polish, "fro")
-        err_norm = .5 * err_norm_row / (n_rows * n_cols) + .5 * err_norm_col / (
-            n_rows * n_cols
-        )
+    err_norm = np.inf
+    n_iter = 0
+
+    while n_iter <= max_iter and err_norm > tol:
+        col_polish = scale(row_polish.T, axis=1, with_mean=with_mean, with_std=with_std)
+        row_polish = scale(col_polish.T, axis=1, with_mean=with_mean, with_std=with_std)
+
+        err_row = np.linalg.norm(rows_X - row_polish, "fro")
+        err_col = np.linalg.norm(cols_X - col_polish, "fro")
+        err_norm = .5 * err_row / (n_rows * n_cols) + .5 * err_col / (n_rows * n_cols)
 
         n_iter += 1
         if verbose:
@@ -93,8 +101,10 @@ def two_way_standardize(
 class TwoWayStandardScaler(BaseEstimator, TransformerMixin):
     """Standardize features by removing the mean and scaling to unit variance
     in both row and column dimensions.
+
     This class is modeled after StandardScaler in scikit-learn.
     Read more in the :ref:`User Guide <preprocessing_scaler>`.
+
     Parameters
     ----------
     copy : boolean, optional, default True
@@ -102,54 +112,51 @@ class TwoWayStandardScaler(BaseEstimator, TransformerMixin):
         This is not guaranteed to always work inplace; e.g. if the data is
         not a NumPy array or scipy.sparse CSR matrix, a copy may still be
         returned.
+
     with_mean : boolean, True by default
         If True, center the data before scaling.
         This does not work (and will raise an exception) when attempted on
         sparse matrices, because centering them entails building a dense
         matrix which in common use cases is likely to be too large to fit in
         memory.
+
     with_std : boolean, True by default
         If True, scale the data to unit variance (or equivalently,
         unit standard deviation).
+
     Attributes
     ----------
     scale_ : ndarray, shape (n_features,)
         Per feature relative scaling of the data.
         .. versionadded:: 0.17
            *scale_*
-    mean_ : array of floats with shape [n_features]
+
+    row_mean_ : array of floats with shape [n_examples]
         The mean value for each feature in the training set.
-    var_ : array of floats with shape [n_features]
+
+    col_mean_ : array of floats with shape [n_features]
+        The mean value for each feature in the training set.
+
+    row_var_ : array of floats with shape [n_examples]
         The variance for each feature in the training set. Used to compute
         `scale_`
+
+    col_var_ : array of floats with shape [n_features]
+        The variance for each feature in the training set. Used to compute
+        `scale_`
+
     n_samples_seen_ : int
         The number of samples processed by the estimator. Will be reset on
         new calls to fit, but increments across ``fit`` calls.
-    Examples
-    --------
-    >>> from inverse_covariance.clean import TwoWayStandardScaler
-    >>>
-    >>> data = [[1, 0], [1, 0], [2, 1], [2, 1]]
-    >>> scaler = StandardScaler()
-    >>> print(scaler.fit(data))
-    StandardScaler(copy=True, with_mean=True, with_std=True)
-    >>> print(scaler.mean_)
-    [ 3.0  0.5]
-    >>> print(scaler.transform(data))
-    [[-1. -1.]
-     [-1. -1.]
-     [ 1.  1.]
-     [ 1.  1.]]
+
     See also
     --------
-    two_way_standardize: Equivalent function without the estimator API.
     :class:`sklearn.preprocessing.StandardScaler`
     :class:`sklearn.decomposition.PCA`
-        Further removes the linear correlation across features with 'whiten=True'.
+
     Notes
     -----
     See the implications of one-way vs. two-way standardization in here. TBD
-
     """
 
     def __init__(self, copy=True, with_mean=True, with_std=True):
@@ -162,15 +169,43 @@ class TwoWayStandardScaler(BaseEstimator, TransformerMixin):
         self.with_std = with_std
         self.copy = copy
 
+    def _reset(self):
+        fit_attrs = [
+            "n_rows_",
+            "n_cols_",
+            "col_var_",
+            "col_mean_",
+            "row_var_",
+            "row_mean_",
+            "row_scale_",
+            "col_scale_",
+            "n_rows_seen_",
+            "n_cols_seen_",
+        ]
+        for attr in fit_attrs:
+            if hasattr(self, attr):
+                delattr(self, attr)
+
+    def _initial_state(self):
+        return not hasattr(self, "n_rows_seen_")
+
     def fit(self, X, y=None):
         """Compute the mean and std for both row and column dimensions.
+
         Parameters
         ----------
         X : {array-like}, shape [n_rows, n_cols]
             The data used to compute the mean and standard deviation
             along both row and column axes
+
         y : Passthrough for ``Pipeline`` compatibility. Input is ignored.
         """
+        # Reset internal state before fitting
+        self._reset()
+        return self.partial_fit(X, y)
+
+    def partial_fit(self, X, y=None):
+        """Online computation of mean and std on X for later scaling."""
         X = check_array(
             X,
             accept_sparse=False,
@@ -187,14 +222,23 @@ class TwoWayStandardScaler(BaseEstimator, TransformerMixin):
 
         self.n_rows_, self.n_cols_ = np.shape(X)
 
-        self.col_var_ = 0. if self.with_std else None
-        self.col_mean_, self.col_var_, self.n_rows_seen_ = _incremental_mean_and_var(
-            X, 0.0, self.col_var_, 0
-        )
+        # Q: This doesnt seem to actually get used in the transform, only in
+        #    the inverse transform which it sounds like we should not support.
 
-        self.row_var_ = 0. if self.with_std else None
+        # initialize variables on first pass
+        if self._initial_state():
+            self.col_mean_ = 0.
+            self.col_var_ = 0. if self.with_std else None
+            self.n_cols_seen_ = 0
+            self.row_mean_ = 0.
+            self.row_var_ = 0. if self.with_std else None
+            self.n_rows_seen_ = 0
+
+        self.col_mean_, self.col_var_, self.n_rows_seen_ = _incremental_mean_and_var(
+            X, self.col_mean_, self.col_var_, self.n_rows_seen_
+        )
         self.row_mean_, self.row_var_, self.n_cols_seen_ = _incremental_mean_and_var(
-            X.T, 0.0, self.row_var_, 0
+            X.T, self.row_mean_, self.row_var_, self.n_cols_seen_
         )
 
         self.row_scale_ = None
@@ -211,6 +255,8 @@ class TwoWayStandardScaler(BaseEstimator, TransformerMixin):
         ----------
         X : array-like, shape [n_rows, n_cols]
             The data used to scale along the features axis.
+        copy : bool, optional (default: False)
+            Copy the input X or not.
         """
         check_is_fitted(self, "row_scale_")
         X = check_array(
@@ -230,16 +276,21 @@ class TwoWayStandardScaler(BaseEstimator, TransformerMixin):
                 "Input is sparse: Algorithm for sparse matrices currently not supported."
             )
 
-        return two_way_standardize(X)
+        return two_way_standardize(
+            X, with_mean=self.with_mean, with_std=self.with_std, copy=self.copy
+        )
 
     def inverse_transform(self, X, copy=False):
         """Scale back the data to the original representation
+
         Parameters
         ----------
         X : array-like, shape [n_samples, n_features]
             The data used to scale along the features axis.
+
         copy : bool, optional (default: False)
             Copy the input X or not.
+
         Returns
         -------
         X_tr : array-like, shape [n_samples, n_features]
@@ -252,6 +303,9 @@ class TwoWayStandardScaler(BaseEstimator, TransformerMixin):
             )
 
         warnings.warn("Reversing two way transformation is not accurate.")
+
+        # Q: Should ^ be a warning or should we just rais here and delete the
+        #    rest of the code?
 
         if copy:
             X = X.copy()
